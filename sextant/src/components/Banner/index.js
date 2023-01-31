6f9e35114e7dece533702a2d6a7e17377c8ba67b
artT14 <lamferme@gmail.com>
2023-01-30 19:23:17 -0800

    Added some extra styling and cisco logo

--- sextant/src/components/Banner/index.js
+++ sextant/src/components/Banner/index.js
@@ -3,6 +3,7 @@ import './styles.css'
 function Banner({title}) {
     return (
     <div className="banner-container">
+        <img alt="logo" src="logo.png" className="banner-logo"/>
         <h1 className="banner">{title}</h1>
     </div>
     )
