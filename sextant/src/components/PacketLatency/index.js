e5d6405e13f169f09949032128360ca573686f30
artT14 <lamferme@gmail.com>
2023-01-30 19:16:21 -0800

    refactored socket connection

--- sextant/src/components/PacketLatency/index.js
+++ sextant/src/components/PacketLatency/index.js
@@ -3,17 +3,21 @@ import { w3cwebsocket as W3CWebSocket } from "websocket";
 import Card from "../Card"
 
 const WS_URL = "ws://localhost:55455"
-const client = new W3CWebSocket(WS_URL);
+
 
 function PacketLatency() {
     const [latency,setLatency] = useState("calculating...")
     
     useEffect(()=>{
+        const client = new W3CWebSocket(WS_URL);
         client.onmessage = (message) => {
             const clientTime = new Date().getTime()
             const serverTime = Number(message.data)
             setLatency(clientTime-serverTime+" ms")
         };
+        return ()=>{
+            client.close()
+        }
     },[])
 
     return (
